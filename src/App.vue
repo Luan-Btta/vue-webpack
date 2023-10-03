b308eb87bc219589b41686d1ac1f795d4175c250
Luan Batista <luanbatista2007@gmail.com>
2023-10-03 00:18:23 -0300

    Feat: Estruturando components e routes

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+
+import Header from "./components/Header";
+import {RouterView} from "vue-router";
+import Sidebar from './components/Sidebar';
+
+export default {
+  components: {
+    Header,
+    Sidebar,
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <Header />
+    <Sidebar />
+    <RouterView />
+  </div>
+</template>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,17 @@
+<script>
+import { RouterLink } from 'vue-router'
+
+export default {
+  components: {
+    RouterLink
+  }
+}
+</script>
+
+
+<template>
+  <ul>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </ul>
+</template>
